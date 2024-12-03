336a4082027f85cef5bd643bde89701e2dc85bda
Данил <dbredelev.dd@gmail.com>
2024-12-03 10:18:54 +0700

    HomeWork1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        var Dog = 8.0;
+        var Cat = 3.6;
+        var Paper = 763789;
+
+        System.out.println(Dog);
+        System.out.println(Cat);
+        System.out.println(Paper);
+
+
+    }
+}
